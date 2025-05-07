918e9edc6fe624545ee0601970f4dda9fab56910
szymakula <szymakula@gmail.com>
2025-05-07 19:21:25 +0200

    refactor: rename file to Navbar

--- src/app/Navbar/index.tsx
+++ src/app/Navbar/index.tsx
@@ -3,7 +3,7 @@ import LinkList from "@/app/Navbar/LinkList";
 import PageLogo from "@/app/Navbar/PageLogo";
 import SideControls from "@/app/Navbar/SideControls";
 
-export default function Index() {
+export default function Navbar() {
   return (
     <MainContentSection>
       <nav className="flex w-full justify-between py-6 relative">
