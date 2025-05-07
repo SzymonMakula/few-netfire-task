f391e68042331d13d158bfb67901e57d3e7e1dc7
szymakula <szymakula@gmail.com>
2025-05-07 23:44:03 +0200

    feat: group together page components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,27 @@
 "use client";
 
+import DiscountBanner, {
+  Discount,
+} from "@/components/organisms/DiscountBanner";
+import Navbar from "@/app/Navbar";
+import ExampleSection from "@/app/ExampleSection";
+import Footer from "@/components/organisms/Footer";
+import Hero from "@/app/Hero";
+
+const MAIN_DISCOUNT: Discount = {
+  value: 15,
+  code: "welcome15",
+  url: new URL("http://localhost"),
+};
+
 export default function Home() {
-  return <div>start</div>;
+  return (
+    <main className="flex items-center flex-col">
+      <DiscountBanner discount={MAIN_DISCOUNT} />
+      <Navbar />
+      <Hero />
+      <ExampleSection />
+      <Footer />
+    </main>
+  );
 }
