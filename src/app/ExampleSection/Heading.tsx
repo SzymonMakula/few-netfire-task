01b7353fd6dfb7873e809c897e16dab9c49eef5b
szymakula <szymakula@gmail.com>
2025-05-07 20:06:02 +0200

    feat: create ExampleSection

---
+++ src/app/ExampleSection/Heading.tsx
@@ -0,0 +1,15 @@
+export default function Heading() {
+  return (
+    <h2 className="text-secondary-400 text-primary-lg text-center leading-10">
+      {"Lorem "}
+      <span className="gradient-text">
+        {
+          "ipsum dolor sit amet consectetur adipiscing elit etiam molestie ex vel"
+        }
+      </span>
+      {
+        "massa lacinia ultricies. Suspendisse lobortis vehicula eros, sed sodales eros mollis eget. Morbi vitae libero in nunc sodales."
+      }
+    </h2>
+  );
+}
---
+++ src/app/ExampleSection/index.tsx
@@ -0,0 +1,24 @@
+import MainContentSection from "@/components/organisms/MainContentSection";
+import Button from "@/components/atoms/Button";
+import Heading from "@/app/ExampleSection/Heading";
+import UnderlineAnchor from "@/components/atoms/UnderlineAnchor";
+
+export default function ExampleSection() {
+  return (
+    <MainContentSection>
+      <div className="flex flex-col items-center pt-15 pb-17 gap-10">
+        <header className="gap-8 flex flex-col items-center">
+          <div>
+            <Button sizing={"lg"} variant={"secondary"}>
+              EXAMPLE SECTION
+            </Button>
+          </div>
+          <Heading />
+        </header>
+        <UnderlineAnchor href={"http://localhost:8000"}>
+          SEE MORE
+        </UnderlineAnchor>
+      </div>
+    </MainContentSection>
+  );
+}
